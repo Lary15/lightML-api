b3761b2259501ea0a2725b2ec705e4945cbadc44
Lary15 <lari1415@hotmail.com>
2022-06-28 20:27:44 +0000

    Update - make all fields on PeripheralReport class optional

--- model/peripherals_report.py
+++ model/peripherals_report.py
@@ -3,11 +3,11 @@ from dataclasses import dataclass
 @dataclass
 class PeripheralsReport:
     """Class for keeping track of all peripherals states (sensors and LED's)"""
-    pir: int
-    ldr_1: int
-    ldr_2: int
-    temp: float
-    mic: float
+    pir: int = 0
+    ldr_1: int = 0
+    ldr_2: int = 0
+    temp: float = 0.0
+    mic: float = 0.0
     led_on: bool = False
     led_color: str = "unknown"
     timestamp: int = 0
