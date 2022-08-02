90394f5c89e9f89df97c828961f24dbfa62b320b
Lary15 <lari1415@hotmail.com>
2022-08-02 00:40:32 -0300

    Add - Service for lamp querying

--- api/services.py
+++ api/services.py
@@ -1,9 +1,22 @@
+from builtins import breakpoint
 import time
+import os
+import requests
+from typing import Tuple
 
-import model
 from model.peripherals_report import PeripheralsReport
 from repository.repository import AbstractRepository
 
 def save_report(report: PeripheralsReport, repo: AbstractRepository):
+    # insert lamp info
+    report.led_on, report.led_color, report.led_dimmer = query_lamp_details()
+    # insert timestamp info
     report.timestamp = int(time.time())
     repo.add(report)
+
+def query_lamp_details() -> Tuple[int, str, int]:
+    try:
+        response = requests.get(f"{os.getenv('LAMP_URL')}/cm?cmnd=HsbColor", timeout=1).json()
+        return int(response["POWER"] == "ON"), response["Color"], response["Dimmer"]
+    except requests.exceptions.ConnectionError:
+        return 0, "0000000000", 0
